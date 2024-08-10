cafed917f35bd18aa1ab6479d9000251672ac086
Athes <athesdrake@gmail.com>
2024-08-10 20:52:33 +0200

    Update rabc

--- src/error.rs
+++ src/error.rs
@@ -17,8 +17,8 @@ pub enum MissingError {
 pub enum UnpackerError {
     #[error("IO error: {0}")]
     IoError(#[from] io::Error),
-    #[error("RABC error: {0:?}")]
-    RabcError(rabc::error::Error),
+    #[error(transparent)]
+    RabcError(#[from] rabc::error::RabcError),
     #[error("utf8 error: {0}")]
     Utf8Error(#[from] std::string::FromUtf8Error),
 
@@ -26,13 +26,4 @@ pub enum UnpackerError {
     KeymapIndexError(u8),
     #[error(transparent)]
     Missing(#[from] MissingError),
-}
-
-impl From<rabc::error::Error> for UnpackerError {
-    fn from(error: rabc::error::Error) -> Self {
-        match error {
-            rabc::error::Error::IoError(error) => Self::IoError(error),
-            error => Self::RabcError(error),
-        }
-    }
 }
